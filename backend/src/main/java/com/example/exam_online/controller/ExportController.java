42b9664d1057f59fc6d95be8bc8646c7827e6227
ndkhang_java <ndkhang.itnlu@gmail.com>
2023-05-18 20:08:52 +0700

    [API] POST Export PDF Handle

--- backend/src/main/java/com/example/exam_online/controller/ExportController.java
+++ backend/src/main/java/com/example/exam_online/controller/ExportController.java
@@ -5,6 +5,7 @@ import com.example.exam_online.dto.ScorePDFExporter;
 import com.example.exam_online.entity.Exam;
 import com.example.exam_online.entity.User;
 import com.example.exam_online.request.ExportScoreRequest;
+import com.example.exam_online.response.ResponseHandler;
 import com.example.exam_online.service.ExamService;
 import com.example.exam_online.service.ResultService;
 import com.example.exam_online.service.UserService;
@@ -31,23 +32,15 @@ public class ExportController {
     @Autowired
     private ExamService examService;
     @PostMapping("/export/pdf")
-    public void exportToPDF(@RequestBody ExportScoreRequest exportScoreRequest, HttpServletResponse response) throws DocumentException, IOException {
-        response.setContentType("application/pdf");
-        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd_HH:mm:ss");
-        String currentDateTime = dateFormatter.format(new Date());
+    public ResponseHandler<List<ScoreBoard>> exportToPDF(@RequestBody ExportScoreRequest exportScoreRequest) throws DocumentException, IOException {
         List<Long> userIds = userService.getUserIds(exportScoreRequest.getRoomId());
         List<User> users = userService.findUserByIds(userIds);
         Exam exam = examService.findById(exportScoreRequest.getExamId());
-        String headerKey = "Content-Disposition";
-        String headerValue = "attachment; filename=score_" + currentDateTime + ".pdf";
-        response.setHeader(headerKey, headerValue);
         List<ScoreBoard> list = new ArrayList<>();
         users.forEach(user -> {
             list.add(new ScoreBoard(user.getIdUser(),user.getUsername(), resultService.getScore(Math.toIntExact(user.getIdUser()), Math.toIntExact(exam.getId())), exam.getTitle()));
         });
 
-        ScorePDFExporter exporter = new ScorePDFExporter(list);
-        exporter.export(response);
-
+return new ResponseHandler<List<ScoreBoard>>("Success", 200, list);
     }
 }
