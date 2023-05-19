942106b68dabf629dc0180410dd9516c1318b7e9
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2023-05-19 14:00:07 +0700

    update validate signup

--- frontend/src/services/accountService.js
+++ frontend/src/services/accountService.js
@@ -7,20 +7,28 @@ import withReactContent from 'sweetalert2-react-content'
 
 export const register = async(user)=>{
         console.log(user);
-        const MySwal = withReactContent(Swal)
-        const res = await request.post("/api/auth/register", {
-            username : user.username,
-            email : user.email,
-            password : user.password,
-        });
-        if(res.data.status === 200){
-            MySwal.fire({  
-                icon: 'success',  
-                title: 'Đăng ký thành công',  
-                showConfirmButton: false,  
-                timer : 1000
-            });  
+        const MySwal = await withReactContent(Swal)
+        try {
+            const res = await request.post("/api/auth/register", {
+                username : user.username,
+                email : user.email,
+                password : user.password,
+            });
+            if(res.data.status === 200){
+                MySwal.fire({  
+                    icon: 'success',  
+                    title: 'Đăng ký thành công',  
+                    showConfirmButton: false,  
+                    timer : 1000
+                });  
+            }
+        } catch(error) {
+            console.log(error)
+            MySwal.fire({
+                icon: 'error',
+                title: error.response.data.message,
+                showConfirmButton: false,                 
+                timer : 3000
+            });
         }
-        
-}
-
+    }
